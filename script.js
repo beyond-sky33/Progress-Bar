0d16524b5a76022c10e7319c47d8dd4082eb1dc3
Badal Kumar <112364403+beyond-sky33@users.noreply.github.com>
2024-02-23 15:51:58 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+let prg = document.querySelector("#progress");
+let h3 = document.querySelector("h3");
+
+let count = 0;
+let int = setInterval(function () {
+    if (count === 100) {
+        h3.style.opacity = 1;
+        clearInterval(int);
+    }
+    count++;
+    prg.style.width = count + '%';
+}, 100);
